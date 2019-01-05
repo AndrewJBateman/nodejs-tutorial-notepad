b44003c8acc5e81592b2870a684813f9fd72becc
Andrew Bateman <gomezbateman@yahoo.com>
2019-01-05 12:49:42 +0100

    assignment 2 added

---
+++ Assignment2/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+//app.use([path,] callback [, callback...])
+//note, path defaults to “/” if no path specified.
+
+//path has to begin with /users
+app.use('/users', (req, res, next) => {
+	console.log('Middleware /users route');
+	res.send('<p>Users page</p>');
+	next();
+});
+
+app.use('/', (req, res, next) => {
+	console.log('Middleware / common route');
+		res.send('<p>Home page</p>');
+});
+
+app.listen(5000);
--- README.md
+++ README.md
@@ -6,4 +6,8 @@ This project uses [node.js](https://nodejs.org) version 10.15.0 LTS.
 
 ## Notes
 
+to do: organise files for Assignment 1.
+
 Link to Udemy Course [NodeJS - The Complete Guide (incl. MVC, REST APIs, GraphQL)](https://www.udemy.com/nodejs-the-complete-guide/).
+Assignment 1: [link to ]()
+Assignment 2: [link to Express documentation on app.use](http://expressjs.com/es/api.html#app.use)
